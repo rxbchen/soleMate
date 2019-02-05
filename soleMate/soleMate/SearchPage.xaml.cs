2e32f184827da5b638486a08b01f64d2da159248
Jenny Jin <jenjenjin@gmail.com>
2019-02-05 16:56:17 -0500

    [search-page] Created toggles for the search page. WIP

--- soleMate/soleMate/SearchPage.xaml.cs
+++ soleMate/soleMate/SearchPage.xaml.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using IntelliAbb.Xamarin.Controls;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace soleMate
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SearchPage : ContentPage
-	{
-		public SearchPage ()
-		{
-			InitializeComponent();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchPage : ContentPage
+    {
+        public SearchPage() => InitializeComponent();
+
+        // Private Variables
+        private string model = "Yeezy";
+        private int size = 8;
+        private string chosen = "";
+
+        private enum PriceRange { Tier1, Tier2, Tier3 };
+        private PriceRange price_range = PriceRange.Tier1;
 
         private async void OnSearchButtonClicked(object sender, EventArgs e)
         {
             //await Navigation.PushAsync(new SearchPage());
         }
 
-        private void CheckmarkButtonClicked(object sender, EventArgs e)
+        private void ModelSwitchToggled(object sender, ToggledEventArgs e)
         {
-            //await Navigation.PushAsync(new SearchPage());
+            chosen = " was chosen";
         }
     }
 }
