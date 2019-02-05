efd14ed2968afb7082960d694770824543dbb72a
bchen <brianchen1997@gmail.com>
2019-02-05 17:48:16 -0500

    Data models

---
+++ soleMate/soleMate/Model/SearchResults.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace soleMate.Model
+{
+    public class SearchResult
+    {
+        public List<Shoe> ShoeList { get; set; }
+
+        public SearchResult()
+        {
+
+        }
+
+    }
+}
---
+++ soleMate/soleMate/Model/Shoe.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace soleMate.Model
+{
+    public class Shoe
+    {
+        public int id { get; set; }
+        public String model { get; set; }
+        public float size { get; set; }
+        public String url { get; set; }
+        public String source { get; set; }
+        public String title { get; set; }
+        public float price { get; set; }
+
+
+        public Shoe()
+        {
+
+        }
+    }
+}
