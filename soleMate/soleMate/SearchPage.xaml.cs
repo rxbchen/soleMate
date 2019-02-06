7c51b6ad8ece47b2fc56053261f05d91d9cf8da5
Jenny Jin <jenjenjin@gmail.com>
2019-02-06 00:42:10 -0500

    [search-page] WIP. Toggle changes, passing data between pages

--- soleMate/soleMate/SearchPage.xaml.cs
+++ soleMate/soleMate/SearchPage.xaml.cs
@@ -1,35 +1,142 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using IntelliAbb.Xamarin.Controls;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace soleMate
-{
+namespace soleMate {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class SearchPage : ContentPage
-    {
-        public SearchPage() => InitializeComponent();
-
-        // Private Variables
-        private string model = "Yeezy";
-        private int size = 8;
-        private string chosen = "";
-
-        private enum PriceRange { Tier1, Tier2, Tier3 };
-        private PriceRange price_range = PriceRange.Tier1;
-
-        private async void OnSearchButtonClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new UnfilteredSearch());
+    public partial class SearchPage : ContentPage {
+        // Constructor
+        public SearchPage() {
+            InitializeComponent();
         }
 
-        private void ModelSwitchToggled(object sender, ToggledEventArgs e)
-        {
-            chosen = " was chosen";
+        // Private Variables
+
+        // TO DO: Change naming of labels (ex. yeezyLabel)
+        private string model = "Yeezy";
+        private int size = 8;
+        private int low_price_range = 0;
+        private int high_price_range = 100;
+        private bool tier1_chosen = false;
+        private bool tier2_chosen = false;
+        private bool tier3_chosen = false;
+
+        // Private Methods
+
+        private async void OnSearchButtonClicked(object sender, EventArgs e) {
+
+            determinePriceRange();
+
+            var shoe = new Model.ShoeSearch {
+                model = model,
+                size = size,
+                low_price = low_price_range,
+                high_price = high_price_range
+            };
+
+            // TODO: Make call to data base here
+            var searchResult = new Model.SearchResult {
+                ShoeList = {
+                    new Model.Shoe {id = 123, model = "Yeezy", price = 1000, size = 8, source = "ebay", title = "BEST YEEZY EVER", url = "https://google.com"},
+                    new Model.Shoe {id = 124, model = "Yeezy", price = 200, size = 8, source = "craigslist", title = "YEET YEEZY", url = "https://google.com"},
+                    new Model.Shoe {id = 125, model = "Yeezy", price = 300, size = 8, source = "ebay", title = "EZ YEEZY", url = "https://google.com"},
+                    new Model.Shoe {id = 126, model = "Yeezy", price = 400, size = 8, source = "kijiji", title = "GET YOUR YEEZY", url = "https://google.com"},
+                    new Model.Shoe {id = 127, model = "Yeezy", price = 700, size = 8, source = "craigslist", title = "PRETTY YEEZY", url = "https://google.com"}
+                }
+            };
+
+            var unfilteredSearchPage = new UnfilteredSearchPage(shoe, searchResult);
+            await Navigation.PushAsync(unfilteredSearchPage);
+        }
+
+        // TO DO: Refactor all toggles into one function and check for type
+        // TO DO: Model should be a bool first, then you check and convert into String
+        private void ModelSwitchToggledYeezy(object sender, ToggledEventArgs e) {
+            if (e.Value) {
+                model = "Yeezy";
+                Yeezy.TextColor = Color.FromHex("#D33F49");
+            } 
+            else {
+                Yeezy.TextColor = Color.Black;
+            }
+        }
+
+        private void ModelSwitchToggledNike(object sender, ToggledEventArgs e) {
+            if (e.Value)
+            {
+                model = "Nike";
+                Nike.TextColor = Color.FromHex("#D33F49");
+            }
+            else
+            {
+                Nike.TextColor = Color.Black;
+            }
+        }
+
+        private void ModelSwitchToggledAdidas(object sender, ToggledEventArgs e) {
+            if (e.Value)
+            {
+                model = "Adidas";
+                Adidas.TextColor = Color.FromHex("#D33F49");
+            }
+            else
+            {
+                Adidas.TextColor = Color.Black;
+            }
+        }
+
+        // TO DO: Refactor all toggle functions into one and check for type
+        // TO DO: Better way for price checks
+
+        private void ModelSwitchToggledTier1(object sender, ToggledEventArgs e) {
+            tier1_chosen = e.Value;
+
+            if (tier1_chosen)
+            {
+                Tier1.TextColor = Color.FromHex("#D33F49");
+            }
+            else
+            {
+                Tier1.TextColor = Color.Black;
+            }
+        }
+
+        private void ModelSwitchToggledTier2(object sender, ToggledEventArgs e) {
+            tier2_chosen = e.Value;
+
+            if (tier2_chosen)
+            {
+                Tier2.TextColor = Color.FromHex("#D33F49");
+
+            }
+            else
+            {
+                Tier2.TextColor = Color.Black;
+            }
+        }
+
+        private void ModelSwitchToggledTier3(object sender, ToggledEventArgs e) {
+            tier3_chosen = e.Value;
+
+            if (tier3_chosen)
+            {
+                Tier3.TextColor = Color.FromHex("#D33F49");
+            }
+            else
+            {
+                Tier3.TextColor = Color.Black;
+            }
+        }
+
+        // TO DO: Refactor into correctly picking prices
+        private void determinePriceRange() { 
+            if (tier1_chosen && tier2_chosen && tier3_chosen) {
+                low_price_range = 0;
+                high_price_range = 600; // Change logic
+            } else { 
+                low_price_range = 0;
+                high_price_range = 100;
+            }
         }
     }
 }
---
+++ soleMate/soleMate/UnfilteredSearchPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace soleMate
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class UnfilteredSearchPage : ContentPage
+	{
+        // Constructors
+        public UnfilteredSearchPage() {
+            InitializeComponent();
+        }
+
+        public UnfilteredSearchPage(Model.ShoeSearch shoeSearch, Model.SearchResult searchResult) {
+            InitializeComponent();
+            this.shoeSearch = shoeSearch;
+            this.searchResult = searchResult;
+            // TO DO: Refactor cause this is terrible lol
+            if (shoeSearch.high_price > 500) {
+                SearchResultText = $"Showing Search Results for: {shoeSearch.model}, Size {shoeSearch.size}, ${shoeSearch.low_price}-$500+";
+            } else { 
+                SearchResultText = $"Showing Search Results for: {shoeSearch.model}, Size {shoeSearch.size}, ${shoeSearch.low_price}-${shoeSearch.high_price}";
+            }
+
+            BindingContext = this;
+        }
+
+        // Private Variables
+        private string SearchResultText { get; }
+        private Model.ShoeSearch shoeSearch = new Model.ShoeSearch();
+        private Model.SearchResult searchResult = new Model.SearchResult();
+
+        // Private Methods
+        private void createGrid() {
+
+            var grid = new Grid
+            {
+                RowDefinitions = {
+
+                }
+            };
+
+            //UnfilteredSearchLayout.Children.Add();
+        }
+
+    }
+}
