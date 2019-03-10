4cf1edb6b90a0825abd1c5ecd277e7253d242bd1
Jenny Jin <jenjenjin@gmail.com>
2019-03-10 00:31:00 -0500

    WIP. Refactoring Search Page to include pickers instead of toggles. Price slider also replaced toggles.

--- soleMate/soleMate/SearchPage.xaml.cs
+++ soleMate/soleMate/SearchPage.xaml.cs
@@ -1,33 +1,60 @@
-﻿using System;
-using soleMate.Model;
-using System.Collections.ObjectModel;
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+﻿namespace soleMate {
+    using System;
+    using soleMate.Model;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+    using System.Collections.Generic;
 
-namespace soleMate {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
     public partial class SearchPage : ContentPage {
+
         // Constructor
+
         public SearchPage() {
             InitializeComponent();
+            IntializeSelectionData();
+            StylePage();
         }
 
         // Private Variables
 
-        // TO DO: Change naming of labels (ex. yeezyLabel)
         private string model = "Yeezy";
         private int size = 8;
         private int low_price_range = 0;
         private int high_price_range = 100;
-        private bool tier1_chosen = false;
-        private bool tier2_chosen = false;
-        private bool tier3_chosen = false;
 
         // Private Methods
 
-        private async void OnSearchButtonClicked(object sender, EventArgs e) {
+        private void IntializeSelectionData() {
+            var modelList = new List<string>();
+            modelList.Add("Yeezy");
+            modelList.Add("Nike");
+            modelList.Add("Adidas");
 
-            determinePriceRange();
+            ModelPicker.ItemsSource = modelList;
+
+            var shoeSizeList = new List<int>();
+            for (int i=5; i<11; i++) {
+                shoeSizeList.Add(i);
+            }
+
+            SizePicker.ItemsSource = shoeSizeList;
+        }
+
+        private void StylePage() {
+            ModelPicker.BackgroundColor = Color.FromHex(Constants.InputField.backgroundColour);
+            SizePicker.BackgroundColor = Color.FromHex(Constants.InputField.backgroundColour);
+            PriceRangeValue.BackgroundColor = Color.FromHex(Constants.InputField.backgroundColour);
+        }
+
+        private void HandlePriceSliderValueChanged(object sender, ValueChangedEventArgs args) {
+            double value = args.NewValue;
+            PriceRangeValue.Text = String.Format("$0 - ${0}", value);
+        }
+
+        private async void OnSearchButtonClicked(object sender, EventArgs e) {
 
             ShoeSearch shoe = new ShoeSearch {
                 model = model,
@@ -48,96 +75,6 @@ namespace soleMate {
 
             UnfilteredSearchPage unfilteredSearchPage = new UnfilteredSearchPage(shoe, searchResult);
             await Navigation.PushAsync(unfilteredSearchPage);
-        }
-
-        // TO DO: Refactor all toggles into one function and check for type
-        // TO DO: Model should be a bool first, then you check and convert into String
-        private void ModelSwitchToggledYeezy(object sender, ToggledEventArgs e) {
-            if (e.Value) {
-                model = "Yeezy";
-                Yeezy.TextColor = Color.FromHex("#D33F49");
-            } 
-            else {
-                Yeezy.TextColor = Color.Black;
-            }
-        }
-
-        private void ModelSwitchToggledNike(object sender, ToggledEventArgs e) {
-            if (e.Value)
-            {
-                model = "Nike";
-                Nike.TextColor = Color.FromHex("#D33F49");
-            }
-            else
-            {
-                Nike.TextColor = Color.Black;
-            }
-        }
-
-        private void ModelSwitchToggledAdidas(object sender, ToggledEventArgs e) {
-            if (e.Value)
-            {
-                model = "Adidas";
-                Adidas.TextColor = Color.FromHex("#D33F49");
-            }
-            else
-            {
-                Adidas.TextColor = Color.Black;
-            }
-        }
-
-        // TO DO: Refactor all toggle functions into one and check for type
-        // TO DO: Better way for price checks
-
-        private void ModelSwitchToggledTier1(object sender, ToggledEventArgs e) {
-            tier1_chosen = e.Value;
-
-            if (tier1_chosen)
-            {
-                Tier1.TextColor = Color.FromHex("#D33F49");
-            }
-            else
-            {
-                Tier1.TextColor = Color.Black;
-            }
-        }
-
-        private void ModelSwitchToggledTier2(object sender, ToggledEventArgs e) {
-            tier2_chosen = e.Value;
-
-            if (tier2_chosen)
-            {
-                Tier2.TextColor = Color.FromHex("#D33F49");
-
-            }
-            else
-            {
-                Tier2.TextColor = Color.Black;
-            }
-        }
-
-        private void ModelSwitchToggledTier3(object sender, ToggledEventArgs e) {
-            tier3_chosen = e.Value;
-
-            if (tier3_chosen)
-            {
-                Tier3.TextColor = Color.FromHex("#D33F49");
-            }
-            else
-            {
-                Tier3.TextColor = Color.Black;
-            }
-        }
-
-        // TO DO: Refactor into correctly picking prices
-        private void determinePriceRange() { 
-            if (tier1_chosen && tier2_chosen && tier3_chosen) {
-                low_price_range = 0;
-                high_price_range = 600; // Change logic
-            } else { 
-                low_price_range = 0;
-                high_price_range = 100;
-            }
         }
     }
 }
