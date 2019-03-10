7833d013224bb20e1f226ea3dd1569ce3b07975e
Jenny Jin <jenjenjin@gmail.com>
2019-03-09 20:55:16 -0500

    Refactored to following better coding guidelines. Styled Main Login and Login Credentials page

--- soleMate/soleMate/MainPage.xaml.cs
+++ soleMate/soleMate/MainPage.xaml.cs
@@ -1,27 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace soleMate
+﻿namespace soleMate
 {
-    public partial class MainPage : ContentPage //This is the login page, for now
-    {
-        public MainPage()
-        {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+    using soleMate.Model;
+
+    /// <summary>
+    /// Login Page summary
+    /// </summary>
+    public partial class MainPage : ContentPage {
+
+        // Constructor
+
+        public MainPage() {
             InitializeComponent();
+            StylePage();
+
         }
 
-        private async void CredentialsButtonClicked(object sender, EventArgs e)
-        {
+        // Private Methods
+
+        private void StylePage() {
+
+            // Buttons
+
+            LoginCredentialsButton.BackgroundColor = Color.FromHex(Constants.Button.mainBackgroundColour);
+            LoginCredentialsButton.HeightRequest = Constants.Button.height;
+            LoginCredentialsButton.WidthRequest = Constants.Button.widthLong;
+
+            LoginFacebookButton.BackgroundColor = Color.FromHex(Constants.Button.mainBackgroundColour);
+            LoginFacebookButton.HeightRequest = Constants.Button.height;
+            LoginFacebookButton.WidthRequest = Constants.Button.widthLong;
+
+            SignUpButton.BackgroundColor = Color.FromHex(Constants.Button.secondaryBackgroundColour);
+            SignUpButton.HeightRequest = Constants.Button.height;
+            SignUpButton.WidthRequest = Constants.Button.widthLong;
+        }
+
+        private async void CredentialsButtonClicked(object sender, EventArgs e) {
             await Navigation.PushAsync(new LoginCredentialsPage());
         }
 
-        private void FacebookButtonClicked(object sender, EventArgs e)
-        {
+        private void FacebookButtonClicked(object sender, EventArgs e) {
             ((Button)sender).Text = "HELLOHELLO";
+            //TODO: Facebook plugin
+        }
+
+        private void SignUpButtonClicked(object sender, EventArgs e) {
+            ((Button)sender).Text = "YAS";
+            //TODO: SignUp plugin
         }
     }
 }
---
+++ soleMate/soleMate/Model/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace soleMate.Model
+{
+    public static class Constants { 
+
+        public static class Button {
+            public const int widthLong = 186;
+            public const int widthShort = 136;
+            public const int height = 42;
+            public const string mainBackgroundColour = "#D33F49";
+            public const string secondaryBackgroundColour = "#0C7C59";
+        }
+
+        public static class InputField {
+            public const int width = 300;
+            public const int height = 42;
+            public const string backgroundColour = "#E5E5E5";
+        }
+    }
+}
